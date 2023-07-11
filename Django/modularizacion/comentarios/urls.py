716f09ae9646795dcd7e62fdde795ced3d5ecbc6
Jose Luis Salazar <jose.luissalacast@gmail.com>
2023-07-11 17:18:27 -0500

    Creación y borrado de datos

--- Django/modularizacion/comentarios/urls.py
+++ Django/modularizacion/comentarios/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("test/", views.test ,name="test" ),
-    path("create/", views.create, name="create")
+    path("create/", views.create, name="create"),
+    path("delete/", views.delete, name="delete")
+    
 
 ]
--- Django/modularizacion/comentarios/views.py
+++ Django/modularizacion/comentarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Comment
 # Create your views here.
 
 def test(request):
@@ -7,4 +8,19 @@ def test(request):
     return HttpResponse("Todo bien")
 
 def create(request):
+    #comment = Comment(name="Jose",score=5,
+                   #   comment="Este es un comentario")
+    
+    #comment.save()
+
+    Comment.objects.create(name="JL")
+
     return HttpResponse("Creando")
+
+def delete(request):
+    #comment = Comment.objects.get(id=1)
+    #comment.delete()
+
+    Comment.objects.filter(id=2).delete()
+
+    return HttpResponse("Borrando")
